987ae14b0170af9adb0a5378eb21819befb7dd39
Siddarth Krishna S <36948033+siddarthkrishna10@users.noreply.github.com>
2019-12-04 13:16:10 +0100

    Corrected the reading a csv bug

--- Part I/Plot_Difference.py
+++ Part I/Plot_Difference.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-b = pd.read_csv('Mean_Table.csv')
+b = pd.read_csv('https://github.com/siddarthkrishna10/Optimisation_Project_Python/blob/master/Part%20I/Mean_Table.csv')
 b.plot(x='Season', y='Average Difference')
 plt.title('The Average Difference Over All Seasons')
 plt.xlabel('Season')
